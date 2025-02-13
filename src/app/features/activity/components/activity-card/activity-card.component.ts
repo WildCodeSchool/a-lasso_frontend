0a72fd80bb404a835ab5be3fbc8716d897800510
sebastienDOC <s.dechand21@gmail.com>
2025-02-13 18:31:10 +0100

    feat: create inscription badge

--- src/app/features/activity/components/activity-card/activity-card.component.ts
+++ src/app/features/activity/components/activity-card/activity-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Activity } from '../../models/activity.model';
+import { InscriptionBadgeComponent } from '../inscription-badge/inscription-badge.component';
 
 @Component({
   selector: 'app-activity-card',
-  imports: [],
+  imports: [InscriptionBadgeComponent],
   templateUrl: './activity-card.component.html',
   styleUrl: './activity-card.component.scss',
+  standalone: true,
 })
 export class ActivityCardComponent {
   @Input() activity!: Activity;
