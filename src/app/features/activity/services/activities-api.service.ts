ff8ce1621facf15c19e263be1e92d7b9e6e4778f
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-02-13 16:59:50 +0100

    create activity card

---
+++ src/app/features/activity/services/activities-api.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { Activity } from '../models/activity.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ActivitiesApiService {
+  _http: HttpClient = inject(HttpClient);
+
+  getAllActivities(): Observable<Activity[]> {
+    // return this._http.get<Activity[]>("/activities");
+    return this._http.get<Activity[]>('/activities/tempActivitiesData.json').pipe(tap((activities: Activity[]) => console.log(activities)));
+  }
+}
