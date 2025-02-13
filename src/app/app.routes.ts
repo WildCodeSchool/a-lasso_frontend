f0ef1f25c04e1f2ec1651777a148a93f2e0b7eae
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-02-13 14:25:02 +0100

    create card activity

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ActivitiesHomeComponent } from './features/activities/pages/activities-home/activities-home.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: AppComponent,
+  },
+  {
+    path: '/activities',
+    component: ActivitiesHomeComponent,
   },
 ];
