4268c6e0838c99cb1b11932ac3269fb681c409d7
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-02-14 13:43:48 +0100

    create footer

---
+++ src/app/features/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {
+  navigate(socialNetwork: string): void {
+    const urls: Record<string, string> = {
+      instagram: 'https://www.instagram.com/',
+      twitter: 'https://www.twitter.com/',
+      facebook: 'https://www.facebook.com/',
+    };
+
+    const url = urls[socialNetwork];
+    if (url) {
+      window.open(url, '_blank');
+    }
+  }
+}
