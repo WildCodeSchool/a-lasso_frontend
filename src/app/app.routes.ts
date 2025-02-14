0fbc0d6ad502e9d86fe8628cdac2e7413dd5609b
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-02-14 10:05:50 +0100

    fix header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,9 @@
 import { Routes } from '@angular/router';
 import { ActivitiesHomeComponent } from './features/activity/pages/activities-home/activities-home.component';
-import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: MainLayoutComponent,
-    children: [
-      {
-        path: 'activities',
-        component: ActivitiesHomeComponent,
-      },
-    ],
+    component: ActivitiesHomeComponent,
   },
 ];
