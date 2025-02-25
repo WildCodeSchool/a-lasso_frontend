4675bc3e90b97f53e47af35ac7d424d5f9b86351
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-02-25 20:14:48 +0100

    finalise connexion back & front for activities and update style cards

--- src/app/features/activity/components/activity-card/activity-card.component.ts
+++ src/app/features/activity/components/activity-card/activity-card.component.ts
@@ -3,10 +3,12 @@ import { Activity } from '../../models/activity.model';
 import { InscriptionBadgeComponent } from '../inscription-badge/inscription-badge.component';
 import { FavoriteHeartComponent } from '../favorite-heart/favorite-heart.component';
 import { environment } from 'src/environments/environment.development';
+import { TruncatePipe } from '../../../../common/Pipes/TruncateString.pipe';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-activity-card',
-  imports: [InscriptionBadgeComponent, FavoriteHeartComponent],
+  imports: [InscriptionBadgeComponent, FavoriteHeartComponent, TruncatePipe, DatePipe],
   templateUrl: './activity-card.component.html',
   styleUrl: './activity-card.component.scss',
   standalone: true,
