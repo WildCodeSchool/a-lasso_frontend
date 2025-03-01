f89585991220528f94a5d4fc0caf86c37df9d55f
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-03-01 17:46:16 +0100

    start create activity details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ActivitiesHomeComponent } from './features/activity/pages/activities-home/activities-home.component';
+import { ActivityDetailsComponent } from './features/activity/pages/activity-details/activity-details.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ActivitiesHomeComponent,
+  },
+  {
+    path: 'activity/:id',
+    component: ActivityDetailsComponent,
   },
 ];
