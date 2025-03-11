78461c7cc1a7b3930a5ef32a0d62be02043fb659
sebastienDOC <s.dechand21@gmail.com>
2025-03-11 16:52:17 +0100

    feat: (issue-23) create reusable single input component

--- src/app/common/components/input-field/input-field.component.ts
+++ src/app/common/components/input-field/input-field.component.ts
@@ -4,11 +4,13 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { FormsModule } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
+import { PasswordModule } from 'primeng/password';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-input-field',
   standalone: true,
-  imports: [CommonModule, InputTextModule, FloatLabelModule, CalendarModule, FormsModule],
+  imports: [CommonModule, InputTextModule, FloatLabelModule, CalendarModule, FormsModule, PasswordModule, DividerModule],
   templateUrl: './input-field.component.html',
   styleUrls: ['./input-field.component.scss'],
 })
@@ -17,5 +19,6 @@ export class InputFieldComponent {
   @Input() type: string = 'text';
   @Input() disabled: boolean = false;
   @Input() variant: 'in' | 'on' = 'in';
+  @Input() showPasswordRules: boolean = false;
   value: string = '';
 }
