18543244deabdfa43c2bd4ea7b5e444b4758388c
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-03-14 11:32:37 +0100

    messages

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  imports: [RouterLink],
 })
 export class HeaderComponent {}
