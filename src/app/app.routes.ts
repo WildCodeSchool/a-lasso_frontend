4b298e0149b698b30372c84f927f9ef2b09a62ba
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-04-17 17:34:23 +0200

    customize popup markers map

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { ActivitiesHomeComponent } from './features/activity/pages/activities-home/activities-home.component';
 import { ActivityDetailsComponent } from './features/activity/pages/activity-details/activity-details.component';
 import { activityDetailsResolver } from './common/resolvers/activity-details.resolver';
+import { AssociationDetailsComponent } from './features/association/pages/association-details/association-details.component';
+import { associationResolver } from './common/resolvers/association.resolver';
 
 export const routes: Routes = [
   {
@@ -13,6 +15,13 @@ export const routes: Routes = [
     component: ActivityDetailsComponent,
     resolve: {
       activityDetails: activityDetailsResolver,
+    },
+  },
+  {
+    path: 'association/:id',
+    component: AssociationDetailsComponent,
+    resolve: {
+      association: associationResolver,
     },
   },
 ];
---
+++ src/app/features/association/pages/association-details/association-details.component.ts
@@ -0,0 +1,55 @@
+import { Component, HostListener, inject, OnInit } from '@angular/core';
+import { ToggleMenuComponent } from '../../../../common/components/toggle-menu/toggle-menu.component';
+import { AssociationCardComponent } from '../../components/association-card/association-card.component';
+import { AsyncPipe, NgClass } from '@angular/common';
+import { ActivityCardComponent } from '../../../activity/components/activity-card/activity-card.component';
+import { Observable } from 'rxjs';
+import { Activity } from '../../../activity/models/activity.model';
+import { ActivityFacadeService } from '../../../activity/services/activity-facade.service';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Association } from '../../models/association.model';
+import { MOBILE_SIZE } from '../../../../common/models/scss-variables';
+
+@Component({
+  selector: 'app-association-details',
+  imports: [ToggleMenuComponent, AssociationCardComponent, NgClass, ActivityCardComponent, AsyncPipe],
+  templateUrl: './association-details.component.html',
+  styleUrl: './association-details.component.scss',
+})
+export class AssociationDetailsComponent implements OnInit {
+  private _activityFacadeService: ActivityFacadeService = inject(ActivityFacadeService);
+  private _route: ActivatedRoute = inject(ActivatedRoute);
+  activities$: Observable<Activity[]> = this._activityFacadeService.activities$;
+  association!: Association;
+
+  navigationItems: string[] = [];
+  chosenNavigation: string = 'Activités';
+  screenWidth: number = window.innerWidth;
+
+  ngOnInit(): void {
+    const associationId = this._route.snapshot.paramMap.get('id');
+    this.association = this._route.snapshot.data['association'];
+    this.activities$ = this.activities$.pipe(map(activities => activities.filter(activity => activity.association.id === associationId)));
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    const target = event.target as Window;
+    this._updateNavigationItems(target.innerWidth);
+    this.screenWidth = target.innerWidth;
+  }
+
+  private _updateNavigationItems(width: number): void {
+    const isMobile: boolean = width < MOBILE_SIZE;
+    if (isMobile) {
+      this.navigationItems = ['Activités', 'Association'];
+    } else {
+      this.navigationItems = [];
+    }
+  }
+
+  handleNavigation(title: string): void {
+    this.chosenNavigation = title;
+  }
+}
