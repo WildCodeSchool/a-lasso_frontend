1bbe221ebbce34f056fb17d2bc782fee9c0d2313
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-05-09 11:12:08 +0200

    fix popup map with new way to handle user interaction

--- src/app/features/map/components/popup-map/popup-map.component.ts
+++ src/app/features/map/components/popup-map/popup-map.component.ts
@@ -1,30 +1,39 @@
-import { Component, ElementRef, inject, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, Input, OnInit, ViewChild } from '@angular/core';
 import { Activity, AssociationActivity } from '../../../activity/models/activity.model';
 import { environment } from '../../../../../environments/environment.development';
 import { TruncatePipe } from '../../../../common/pipes/TruncateString.pipe';
-import { DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 import { InscriptionBadgeComponent } from '../../../activity/components/inscription-badge/inscription-badge.component';
 import { FavoriteHeartComponent } from '../../../activity/components/favorite-heart/favorite-heart.component';
 import { UUIDTypes } from 'uuid';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { selectActivities } from '../../../activity/store/activities.selector';
-import { take } from 'rxjs';
+import { Observable, take } from 'rxjs';
+import { ActivityFacadeService } from '../../../activity/services/activity-facade.service';
 
 @Component({
   selector: 'app-popup-map',
-  imports: [TruncatePipe, InscriptionBadgeComponent, DatePipe, FavoriteHeartComponent],
+  imports: [TruncatePipe, InscriptionBadgeComponent, DatePipe, FavoriteHeartComponent, AsyncPipe],
   templateUrl: './popup-map.component.html',
   styleUrl: './popup-map.component.scss',
 })
-export class PopupMapComponent {
+export class PopupMapComponent implements OnInit {
   private _router: Router = inject(Router);
   private _store: Store = inject(Store);
+  private _activityFacadeService: ActivityFacadeService = inject(ActivityFacadeService);
+
   @Input() activity!: Activity;
   @Input() association!: AssociationActivity;
   @ViewChild('content', { static: true }) content!: ElementRef;
+
+  public isSavedActivity$: Observable<boolean>;
   public apiUrl: string = environment.apiUrl;
   public activityCountByAssociation: number = 0;
+
+  ngOnInit(): void {
+    this.isSavedActivity$ = this._activityFacadeService.getIsSavedActivity(this.activity.id);
+  }
 
   public setCountActivityOfAssociation(association: AssociationActivity): void {
     this.association = association;
