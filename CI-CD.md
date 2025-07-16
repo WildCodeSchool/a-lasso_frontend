a92b9e877c6f3a5393e4b7ebd4a1e79d003fa640
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-07-16 12:04:04 +0200

    CI CD works !... for development

---
+++ CI-CD.md
@@ -0,0 +1,3 @@
+##Travail en local
+
+'''docker compose -f docker-compose.development.yml up'''
--- src/app/features/authentication/store/user.selectors.ts
+++ src/app/features/authentication/store/user.selectors.ts
@@ -26,4 +26,4 @@ export const selectAssociationFounder = createSelector(selectUser, user => (user
 
 export const selectAssociationDescription = createSelector(selectUser, user => (user?.type === UserType.Association ? user.description : ''));
 
-export const selectAssociationStats = createSelector(selectUser, user => (user?.type === UserType.Association ? (user.statistics ?? []) : []));
+export const selectAssociationStats = createSelector(selectUser, user => (user?.type === UserType.Association ? user.statistics ?? [] : []));
