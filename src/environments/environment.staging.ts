6d5076b27f681a2b00bd75e9053316035c795ddb
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-08-25 12:55:56 +0200

    test full aide pour la ci

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: false,
   name: 'staging',
-  apiUrl: 'https://staging-api.a-l-asso.fr',
+  apiUrl: 'http://alasso-spring-app-staging-ci:8080',
+  MYSQL_ROOT_PASSWORD: 'mysql',
+  DB_NAME_STAGING: 'stagingdb',
+  DB_USER_STAGING: 'user_staging',
+  DB_PASSWORD_STAGING: 'password_staging',
+  DB_HOST_STAGING: 'alasso-mysql-staging-ci',
+  DB_PORT_STAGING: 3306,
 };
