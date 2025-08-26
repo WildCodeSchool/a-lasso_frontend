eb31841adfea75ed33a9defe3693af8ae45e29a2
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-08-26 11:59:37 +0200

    fix dockerfile

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,10 +2,4 @@ export const environment = {
   production: false,
   name: 'staging',
   apiUrl: 'http://alasso-spring-app-staging-ci:8080',
-  MYSQL_ROOT_PASSWORD: 'mysql',
-  DB_NAME_STAGING: 'stagingdb',
-  DB_USER_STAGING: 'user_staging',
-  DB_PASSWORD_STAGING: 'password_staging',
-  DB_HOST_STAGING: 'alasso-mysql-staging-ci',
-  DB_PORT_STAGING: 3306,
 };
